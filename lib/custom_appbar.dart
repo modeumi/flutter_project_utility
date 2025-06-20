8a05465aadab4aa95e03e93330446e343e460ca9
Kang HoJung <hojung.kang@seoul-soft.com>
2025-06-20 10:28:41 +0900

    동기화

--- lib/custom_appbar.dart
+++ lib/custom_appbar.dart
@@ -21,48 +21,53 @@ class CustomAppbar extends StatefulWidget implements PreferredSizeWidget {
   State<CustomAppbar> createState() => _CustomAppbarState();
 
   @override
-  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+  Size get preferredSize => const Size.fromHeight(kMinInteractiveDimension);
 }
 
 class _CustomAppbarState extends State<CustomAppbar> {
   @override
   Widget build(BuildContext context) {
-    return AppBar(
-      backgroundColor: widget.color ?? Colors.white,
-      primary: false,
-      title: SizedBox(
-          height: 30,
-          child: Center(
-              child: widget.type
-                  ? Image.asset(
-                      widget.content,
-                      fit: BoxFit.fitHeight,
-                    )
-                  : asText(widget.content, black(20, FontWeight.w600)))),
-      centerTitle: true,
-      leading: GestureDetector(
-        onTap: () {
-          if (widget.back_action != null) {
-            widget.back_action!();
-          } else {
-            Get.back();
-          }
-        },
-        child: const Icon(
-          Icons.arrow_back_ios_new,
-          size: 30,
-          color: Colors.black,
+    return Container(
+      padding: EdgeInsets.only(top: MediaQuery.paddingOf(context).top),
+      color: widget.color ?? Colors.white,
+      child: AppBar(
+        surfaceTintColor: Colors.transparent,
+        backgroundColor: Colors.transparent,
+        primary: false,
+        title: SizedBox(
+            height: 30,
+            child: Center(
+                child: widget.type
+                    ? Image.asset(
+                        widget.content,
+                        fit: BoxFit.fitHeight,
+                      )
+                    : asText(widget.content, black(20, FontWeight.w600)))),
+        centerTitle: true,
+        leading: GestureDetector(
+          onTap: () {
+            if (widget.back_action != null) {
+              widget.back_action!();
+            } else {
+              Get.back();
+            }
+          },
+          child: const Icon(
+            Icons.arrow_back_ios_new,
+            size: 30,
+            color: Colors.black,
+          ),
         ),
+        actions: [
+          IconButton(
+              onPressed: widget.action,
+              icon: const Icon(
+                Icons.home_outlined,
+                color: Colors.black,
+                size: 30,
+              )),
+        ],
       ),
-      actions: [
-        IconButton(
-            onPressed: widget.action,
-            icon: const Icon(
-              Icons.home_outlined,
-              color: Colors.black,
-              size: 30,
-            )),
-      ],
     );
   }
 }
