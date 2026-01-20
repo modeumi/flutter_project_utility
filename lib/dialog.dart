2b41b6329bc2bdf4bf55b7249f85b31fdc4dc1cd
Kang HoJung <hojung.kang@seoul-soft.com>
2026-01-20 09:41:35 +0900

    update utility

--- lib/dialog.dart
+++ lib/dialog.dart
@@ -3,7 +3,7 @@ import 'package:utility/textstyle.dart';
 import 'package:flutter/material.dart';
 
 Widget Show_Dialog(Widget title, Widget content, VoidCallback action, VoidCallback cancle, TextStyle approval,
-    [bool? disable_cancle, String? apply_text, String? cancle_text]) {
+    [bool? disable_cancle, String? apply_text, String? cancle_text, bool? disableButton]) {
   return Dialog(
     insetPadding: const EdgeInsets.symmetric(horizontal: 20),
     elevation: 10,
@@ -25,78 +25,79 @@ Widget Show_Dialog(Widget title, Widget content, VoidCallback action, VoidCallba
               ),
               child: content,
             ),
-            Container(
-              width: double.infinity,
-              height: 50,
-              decoration: BoxDecoration(border: Border(top: BorderSide(width: 1, color: Colors.grey[300]!))),
-              child: disable_cancle ?? false
-                  ? GestureDetector(
-                      onTap: () {
-                        action();
-                      },
-                      child: Container(
-                        color: Colors.transparent,
-                        width: double.infinity,
-                        height: double.infinity,
-                        child: Center(
-                          child: Text(
-                            apply_text ?? '확인',
-                            textAlign: TextAlign.center,
-                            style: approval,
-                          ),
-                        ),
-                      ),
-                    )
-                  : Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Flexible(
-                          flex: 5,
-                          child: GestureDetector(
-                            onTap: () {
-                              cancle();
-                            },
-                            child: Container(
-                              color: Colors.transparent,
-                              height: double.infinity,
-                              width: double.infinity,
-                              child: Center(
-                                child: Text(
-                                  cancle_text ?? '취소',
-                                  textAlign: TextAlign.center,
-                                  style: grey(18, FontWeight.w500),
-                                ),
-                              ),
-                            ),
-                          ),
-                        ),
-                        Container(
+            if ((disableButton ?? false) == false)
+              Container(
+                width: double.infinity,
+                height: 50,
+                decoration: BoxDecoration(border: Border(top: BorderSide(width: 1, color: Colors.grey[300]!))),
+                child: disable_cancle ?? false
+                    ? GestureDetector(
+                        onTap: () {
+                          action();
+                        },
+                        child: Container(
+                          color: Colors.transparent,
+                          width: double.infinity,
                           height: double.infinity,
-                          decoration: BoxDecoration(border: Border(right: BorderSide(color: Colors.grey[300]!, width: 1))),
+                          child: Center(
+                            child: Text(
+                              apply_text ?? '확인',
+                              textAlign: TextAlign.center,
+                              style: approval,
+                            ),
+                          ),
                         ),
-                        Flexible(
-                          flex: 5,
-                          child: GestureDetector(
-                            onTap: () {
-                              action();
-                            },
-                            child: Container(
-                              color: Colors.transparent,
-                              width: double.infinity,
-                              height: double.infinity,
-                              child: Center(
-                                child: Text(
-                                  apply_text ?? '확인',
-                                  textAlign: TextAlign.center,
-                                  style: approval,
+                      )
+                    : Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Flexible(
+                            flex: 5,
+                            child: GestureDetector(
+                              onTap: () {
+                                cancle();
+                              },
+                              child: Container(
+                                color: Colors.transparent,
+                                height: double.infinity,
+                                width: double.infinity,
+                                child: Center(
+                                  child: Text(
+                                    cancle_text ?? '취소',
+                                    textAlign: TextAlign.center,
+                                    style: grey(18, FontWeight.w500),
+                                  ),
                                 ),
                               ),
                             ),
                           ),
-                        ),
-                      ],
-                    ),
-            )
+                          Container(
+                            height: double.infinity,
+                            decoration: BoxDecoration(border: Border(right: BorderSide(color: Colors.grey[300]!, width: 1))),
+                          ),
+                          Flexible(
+                            flex: 5,
+                            child: GestureDetector(
+                              onTap: () {
+                                action();
+                              },
+                              child: Container(
+                                color: Colors.transparent,
+                                width: double.infinity,
+                                height: double.infinity,
+                                child: Center(
+                                  child: Text(
+                                    apply_text ?? '확인',
+                                    textAlign: TextAlign.center,
+                                    style: approval,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+              )
           ],
         ),
       ),
